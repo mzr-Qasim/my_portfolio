c0e20791b389947232d5164aa0c5f82c79bebe10
Muhammad Qasim <malikqasim20051@gmail.com>
2024-11-15 17:44:40 +0500

    white noise settings

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 MEDIA_URL = '/media/'
