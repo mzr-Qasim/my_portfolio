bc6312ae5eff088d5b8f1b8de91ce88925c20e08
Muhammad Qasim <malikqasim20051@gmail.com>
2024-11-15 17:42:38 +0500

    white noise settings

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6lt9+u990d(1zd+!o*f-eqg^9gu11v#bfysp49259v+y4ohbkl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['malikqasim.info', 'www.malikqasim.info']
 
  
 # Application definition
